8c0af41a2d3a4383bb4c5755b084b808a3f072bc
Wojciech Lehmann <lehmann.wojciech@icloud.com>
2020-12-18 09:41:21 +0100

    fixes

--- swiper.js
+++ swiper.js
@@ -1,6 +1,15 @@
 new Swiper(".swiper-container", {
-      speed: 700,
+      speed: 1000,
       spaceBetween: 100,
+      effect: "slide",
+      slidesPerView: "1",
+      autoplay: {
+            delay: 2500,
+      },
+      loop: 'true',
+      // flipEffect: {
+      //       slideShadows: false,
+      // },
       navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev"
