f74bef1ad15ee9460e600a813325de739f9afb3c
Wojciech Lehmann <lehmann.wojciech@icloud.com>
2020-12-19 17:47:03 +0100

    flip effect gallery

--- swiper.js
+++ swiper.js
@@ -1,15 +1,13 @@
 new Swiper(".swiper-container", {
       speed: 1000,
       spaceBetween: 100,
-      effect: "slide",
+      effect: "flip",
       slidesPerView: "1",
-      autoplay: {
-            delay: 2500,
-      },
+
       loop: 'true',
-      // flipEffect: {
-      //       slideShadows: false,
-      // },
+      flipEffect: {
+            slideShadows: false,
+      },
       navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev"
